25fdb1b0de186207ba5230e2cc3cf7b7a58e49f2
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-08-11 21:21:51 +0300

    fix support

--- frontend/src/app/features/support/component/support.component.ts
+++ frontend/src/app/features/support/component/support.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+  ],
   templateUrl: './support.component.html',
-  styleUrl: './support.component.scss'
+  styleUrl: './support.component.scss',
 })
 export class SupportComponent {
+  // Потом просто подставишь реальные контакты
+  productsPhone = '+7 (928) 536-00-09';
+  productsEmail = 'amirseidova@bk.ru';
 
+  technicalEmail = 'abakarmadatov2@gmail.com';
+  technicalTelegram = '@ngdrx';
 }
