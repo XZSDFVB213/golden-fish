c61b69d64e743411f39c83988fbc6efda0e71b2b
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-08-11 21:14:55 +0300

    fix profile

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,22 +1,33 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { MatCard } from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
-import { IUser } from '../../shared/models/user/user.interface';
-import { AuthService } from '../../core/services/auth/auth.service';
+import { Component, computed, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+
+import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
+
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatIcon, MatCard, RouterLink, MatButton],
+  imports: [
+    MatIcon,
+    RouterLink,
+    MatButton,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
-  user = signal<IUser | null>(this.authService.user());
+
+  user = this.authService.user;
+
+  initial = computed(() => {
+    return this.user()?.name
+      ?.charAt(0)
+      .toUpperCase() ?? 'П';
+  });
 
   logout() {
     this.authService.logout().subscribe(() => {
