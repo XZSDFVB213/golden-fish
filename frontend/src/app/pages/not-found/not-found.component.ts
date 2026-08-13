967ad5388e79a2a4b1a71eccdc79815224a7549c
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-08-13 15:22:12 +0300

    fix 404 notfound

--- frontend/src/app/pages/not-found/not-found.component.ts
+++ frontend/src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatIconModule,
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
