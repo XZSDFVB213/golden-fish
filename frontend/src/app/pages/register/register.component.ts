32eb92d6e0f22d24abe908b626843ae87f61bfc6
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-08-13 14:07:03 +0300

    fix register

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AuthService } from '../../core/services/auth/auth.service';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIcon } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,12 +14,9 @@ import { Router, RouterLink } from '@angular/router';
   imports: [
     ReactiveFormsModule,
     RouterLink,
-
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIcon,
+    MatIconModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
@@ -31,11 +27,15 @@ export class RegisterComponent {
   private router = inject(Router);
 
   isLoading = false;
+
   errorMessage = signal('');
+  hidePassword = signal(true);
 
   form = this.fb.nonNullable.group({
-    name: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.minLength(2)]],
+
     email: ['', [Validators.required, Validators.email]],
+
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
@@ -46,6 +46,7 @@ export class RegisterComponent {
     }
 
     this.isLoading = true;
+    this.errorMessage.set('');
 
     this.authService
       .main('register', {
@@ -55,16 +56,21 @@ export class RegisterComponent {
         next: () => {
           this.router.navigate(['/home']);
         },
-        error: (err: { error: { message: any } }) => {
-          this.errorMessage.set(err.error?.message || 'Произошла ошибка');
+
+        error: (err) => {
+          this.errorMessage.set(
+            err.error?.message || 'Не удалось создать аккаунт',
+          );
 
           this.isLoading = false;
         },
+
         complete: () => {
           this.isLoading = false;
         },
       });
   }
+
   loginWithGoogle() {
     this.authService.googleLogin();
   }
