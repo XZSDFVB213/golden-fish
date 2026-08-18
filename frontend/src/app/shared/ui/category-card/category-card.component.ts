f6e252ee9e691552dc74f4e158abd633e0b79e63
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-08-18 15:12:41 +0300

    try new design

--- frontend/src/app/shared/ui/category-card/category-card.component.ts
+++ frontend/src/app/shared/ui/category-card/category-card.component.ts
@@ -11,20 +11,43 @@ import { ICategory } from '../../models/category/category.interface';
 @Component({
   selector: 'app-category-card',
   standalone: true,
-  imports: [
-    RouterLink,
-  ],
+  imports: [RouterLink],
   templateUrl: './category-card.component.html',
   styleUrl: './category-card.component.scss',
 })
 export class CategoryCardComponent {
   category = input.required<ICategory>();
-
   index = input(0);
 
-  variant = computed(
-    () => `variant-${(this.index() % 5) + 1}`,
-  );
+  image = computed(() => {
+    const title =
+      this.category().title.toLowerCase();
+
+    if (title.includes('рыб')) {
+      return 'assets/categories/fish-category.png';
+    }
+
+    if (
+      title.includes('морепродукт') ||
+      title.includes('кревет') ||
+      title.includes('кальмар')
+    ) {
+      return 'assets/categories/seafood.png';
+    }
+
+    if (title.includes('икр')) {
+      return 'assets/categories/caviar.png';
+    }
+
+    if (
+      title.includes('кулинар') ||
+      title.includes('готов')
+    ) {
+      return 'assets/categories/cooking.png';
+    }
+
+    return 'assets/categories/default.png';
+  });
 
   onClick() {}
 }
