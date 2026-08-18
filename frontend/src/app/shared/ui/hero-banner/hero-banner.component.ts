53b1ac1831521ed17d35e1b71d0b169f1836ebbe
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-08-18 14:56:55 +0300

    try new design

--- frontend/src/app/shared/ui/hero-banner/hero-banner.component.ts
+++ frontend/src/app/shared/ui/hero-banner/hero-banner.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-banner',
   standalone: true,
-  imports: [MatButton,RouterLink, MatIconModule],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './hero-banner.component.html',
-  styleUrl: './hero-banner.component.scss'
+  styleUrl: './hero-banner.component.scss',
 })
-export class HeroBannerComponent {
-
-}
+export class HeroBannerComponent {}
